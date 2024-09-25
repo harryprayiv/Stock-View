470aec8b50029edc668045621704760dafbade03
harryprayiv <harryprayiv@gmail.com>
2024-09-25 14:04:44 -0400

    massive refactor in progress

--- src/BudView.purs
+++ src/BudView.purs
@@ -16,16 +16,20 @@ import Fetch.Yoga.Json (fromJSON)
 import Foreign (Foreign, ForeignError)
 import Foreign.Index (readProp)
 import JS.Fetch.RequestBody as RB
-import Yoga.JSON (class ReadForeign, class WriteForeign, readImpl, unsafeStringify, writeImpl)
+import Yoga.JSON (class ReadForeign, readImpl, unsafeStringify, writeImpl)
 
+-- ForeignRequestBody to wrap Foreign
+newtype ForeignRequestBody = ForeignRequestBody Foreign
+
+-- Define InventoryResponse type
 data InventoryResponse
   = InventoryData Inventory
   | Message String
 
-newtype ForeignRequestBody = ForeignRequestBody Foreign
-
+-- Define Inventory as a newtype
 newtype Inventory = Inventory (Array MenuItem)
 
+-- Define MenuItem as a newtype over a record
 newtype MenuItem = MenuItem
   { name :: String
   , category :: String
@@ -36,8 +40,9 @@ newtype MenuItem = MenuItem
   , quantity :: Int
   }
 
-instance writeForeignMenuItem :: WriteForeign MenuItem where
-  writeImpl (MenuItem item) = writeImpl item
+instance toRequestBodyForeignRequestBody :: ToRequestBody ForeignRequestBody where
+  toRequestBody (ForeignRequestBody foreignValue) =
+    RB.fromString (unsafeStringify foreignValue)
 
 instance readForeignMenuItem :: ReadForeign MenuItem where
   readImpl json = do
@@ -50,57 +55,44 @@ instance readForeignMenuItem :: ReadForeign MenuItem where
     quantity <- readProp "quantity" json >>= readImpl
     pure $ MenuItem { name, category, subcategory, species, sku, price, quantity }
 
-instance ToRequestBody ForeignRequestBody where
-  toRequestBody (ForeignRequestBody foreignValue) =
-    RB.fromString (unsafeStringify foreignValue)
-
 instance readForeignInventory :: ReadForeign Inventory where
   readImpl json = do
     items <- readImpl json :: ExceptT (NonEmptyList ForeignError) Identity (Array MenuItem)
     pure (Inventory items)
 
-instance showMenuItem :: Show MenuItem where
-  show (MenuItem item) =
-    "MenuItem { name: " <> show item.name <>
-    ", category: " <> show item.category <>
-    ", subcategory: " <> show item.subcategory <>
-    ", species: " <> show item.species <>
-    ", sku: " <> show item.sku <>
-    ", price: " <> show item.price <>
-    ", quantity: " <> show item.quantity <> " }"
-
-instance showInventory :: Show Inventory where
-  show (Inventory items) = "Inventory " <> show items
-
-instance showInventoryResponse :: Show InventoryResponse where
-  show (InventoryData inventory) = "InventoryData " <> show inventory
-  show (Message msg) = "Message " <> show msg
-
+-- Fetch Inventory from Local JSON
 fetchInventoryFromJson :: Aff (Either String InventoryResponse)
 fetchInventoryFromJson = do
   result <- attempt do
     timestamp <- liftEffect $ show <$> now
     let url = "/inventory.json?t=" <> timestamp
     liftEffect $ log ("Fetching URL: " <> url)
+    
     coreResponse <- fetch url {}
     inventory <- fromJSON coreResponse.json :: Aff Inventory
+    
     pure inventory
+
   case result of
     Left err -> pure $ Left $ "Fetch error: " <> show err
     Right inventory -> pure $ Right $ InventoryData inventory
 
+-- Fetch Inventory from HTTP API
 fetchInventoryFromHttp :: Aff (Either String InventoryResponse)
 fetchInventoryFromHttp = do
   result <- attempt do
     let requestHeaders = { "Content-Type": "application/json" }
     let requestBody = ForeignRequestBody (writeImpl { hello: "world" })
+    
     coreResponse <- fetch "https://httpbin.org/post"
       { method: POST
       , body: requestBody
       , headers: requestHeaders
       }
+    
     res <- fromJSON coreResponse.json :: Aff Foreign
     pure $ "Received response: " <> unsafeStringify res
+
   case result of
     Left err -> pure $ Left $ "Fetch error: " <> show err
     Right msg -> pure $ Right $ Message msg
--- src/Render.purs
+++ src/Render.purs
@@ -2,24 +2,22 @@ module Render where
 
 import Prelude
 
-import BudView (Inventory(..), InventoryResponse(..), MenuItem(..), fetchInventoryFromJson)
-import Control.Monad.Rec.Class (forever)
+import BudView (Inventory(..), InventoryResponse(..), MenuItem(..), fetchInventoryFromJson, fetchInventoryFromHttp)
 import Data.Array (filter, sortBy)
 import Data.Either (Either(..))
 import Data.Tuple.Nested ((/\))
-import Deku.Attributes (klass_)
-import Deku.Control (text_)
-import Deku.Core (Nut(..))
+import Deku.Core (Nut(..), text_)
 import Deku.DOM as D
+import Deku.DOM.Attributes (klass_)
 import Deku.Do as Deku
-import Deku.Hooks (useDyn, useState)
+import Deku.Effect (useState)
+import Deku.Hooks (useDyn)
 import Deku.Toplevel (runInBody)
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_, delay)
-import Effect.Aff.Class (liftAff)
-import Effect.Class.Console (log)
-import Effect.Timer (setInterval)
+import Effect.Aff (Aff, launchAff)
 import Effect.Class (liftEffect)
+import Effect.Class.Console (log)
+import FRP.Event.Time (interval')
 
 -- Sorting Configuration
 data SortField = SortByName | SortByCategory | SortBySubCategory | SortByPrice | SortByQuantity
@@ -75,12 +73,18 @@ renderItem (MenuItem item) = D.div
   , D.div [] [ text_ ("Quantity: " <> show item.quantity) ]
   ]
 
--- Poll function for fetching inventory periodically
-fetchInventoryPoll :: Aff (Either String InventoryResponse)
-fetchInventoryPoll = fetchInventoryFromJson
+-- Switch between Local JSON and HTTP Fetch
+fetchInventory :: String -> Aff (Either String InventoryResponse)
+fetchInventory mode = do
+  log "Fetching inventory..."
+  result <- case mode of
+    "json" -> fetchInventoryFromJson
+    "http" -> fetchInventoryFromHttp
+    _ -> pure $ Left "Invalid mode"
+  pure result
 
 app :: Effect Unit
-app = void runInBody Deku.do
+app = void $ runInBody Deku.do
   -- Initialize inventory state
   setInventory /\ inventory <- useState (Inventory [])
 
@@ -91,18 +95,22 @@ app = void runInBody Deku.do
         , screens: 1
         }
 
-  -- Function to fetch inventory periodically
-  liftAff $ launchAff_ $ do
-    setInterval 3000 $ do -- 3000 ms interval
-      result <- fetchInventoryPoll
-      case result of
-        Left err -> log ("Error: " <> err)
-        Right (InventoryData inv) -> liftEffect $ setInventory inv
-        Right (Message msg) -> log ("Message: " <> msg)
-      pure unit
+  -- Start polling for inventory updates every 3000ms using liftEffect
+  launchAff $ do
+    void $ interval' (liftEffect (pollInventory setInventory)) 3000
 
   -- Render inventory when it changes
   do
     { value: inv } <- useDyn inventory
     renderInventory config inv
     pure unit
+
+  where
+    pollInventory :: (Inventory -> Effect Unit) -> Aff Unit
+    pollInventory setInventory = do
+      let mode = "json"  -- Switch between "json" and "http"
+      result <- fetchInventory mode
+      case result of
+        Left err -> log ("Error: " <> err)
+        Right (InventoryData inv) -> liftEffect $ setInventory inv
+        _ -> pure unit
